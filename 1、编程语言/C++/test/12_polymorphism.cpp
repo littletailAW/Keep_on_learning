191a83715cde38c58b0c347718c53dc6744e5833
littletail <littletail1006@qq.com>
2021-03-06 00:08:25 +0800

    1、加入两个多态案例

--- 1、编程语言/C++/test/12_polymorphism.cpp
+++ 1、编程语言/C++/test/12_polymorphism.cpp
@@ -3,7 +3,7 @@
  * @version: 0.0
  * @Author: Littletail
  * @Date: 2021-03-05 14:40:43
- * @LastEditTime: 2021-03-05 20:32:41
+ * @LastEditTime: 2021-03-06 00:06:26
  */
 #include <iostream>
 #include <windows.h>
@@ -90,6 +90,7 @@ void test2()
 }
 #endif
 /* 2、多态实现 */
+#if 0
 class Calculator
 {
 public:
@@ -124,13 +125,193 @@ void test3()
     cout << c->m_num1 << "+" << c->m_num2 << "= " << c->get_res() << endl;
     delete c;
 }
+#endif
+/* 多态案例2*/
+//给老师来一杯卡布奇诺
+#if 0
+class AbstractDrinking
+{
+public:
+    /* 1.煮水*/
+    virtual void boil() = 0;
+    /* 2.冲泡*/
+    virtual void brew() = 0;
+    /* 3.倒入杯子里*/
+    virtual void input_cap() = 0;
+    /* 4.加入东西*/
+    virtual void add_thing() = 0;
+    /* 5.制作成品*/
+    void make_drink()
+    {
+        boil();
+        brew();
+        input_cap();
+        add_thing();
+    }
+};
+//咖啡
+class coffee : public AbstractDrinking
+{
+    virtual void boil()
+    {
+        cout << "煮矿泉水" << endl;
+    }
+
+    virtual void brew()
+    {
+        cout << "冲咖啡" << endl;
+    }
+
+    virtual void input_cap()
+    {
+        cout << "倒入杯子" << endl;
+    }
+
+    virtual void add_thing()
+    {
+        cout << "加入牛奶" << endl;
+    }
+};
+//茶水
+class tea : public AbstractDrinking
+{
+    virtual void boil()
+    {
+        cout << "煮矿泉水" << endl;
+    }
+
+    virtual void brew()
+    {
+        cout << "泡茶叶" << endl;
+    }
+
+    virtual void input_cap()
+    {
+        cout << "倒入杯子" << endl;
+    }
+
+    virtual void add_thing()
+    {
+        cout << "加入大枣" << endl;
+    }
+};
+//制作
+void do_drink(AbstractDrinking *abs)
+{
+    abs->make_drink();
+    delete abs;
+}
+void test4()
+{
+    do_drink(new coffee);
+    do_drink(new tea);
+}
+#endif
+/* 多态案例3*/
+#if 1
+/* CPU*/
+class CPU
+{
+public:
+    //抽象的计算函数
+    virtual void calculate() = 0;
+};
+/* 显卡*/
+class Video
+{
+public:
+    //抽象的显示函数
+    virtual void display() = 0;
+};
+/* 内存条*/
+class memory
+{
+public:
+    //抽象的存储函数
+    virtual void storage() = 0;
+};
+
+//电脑
+class computer
+{
+public:
+    computer(CPU *cpu, Video *vi, memory *mem)
+    {
+        m_cpu = cpu;
+        m_vi = vi;
+        m_mem = mem;
+    }
+    void work()
+    {
+        m_cpu->calculate();
+        m_vi->display();
+        m_mem->storage();
+    }
+    ~computer()
+    {
+        if (NULL != m_cpu)
+        {
+            delete m_cpu;
+            m_cpu = NULL;
+        }
+        if (NULL != m_vi)
+        {
+            delete m_vi;
+            m_vi = NULL;
+        }
+        if (NULL != m_mem)
+        {
+            delete m_mem;
+            m_mem = NULL;
+        }
+    }
+
+private:
+    CPU *m_cpu;
+    Video *m_vi;
+    memory *m_mem;
+};
+class inter_cpu : public CPU
+{
+    virtual void calculate()
+    {
+        cout << "inter cpu doing" << endl;
+    }
+};
+class inter_vi : public Video
+{
+    virtual void display()
+    {
+        cout << "inter video doing" << endl;
+    }
+};
+class inter_mem : public memory
+{
+    virtual void storage()
+    {
+        cout << "inter mem doing" << endl;
+    }
+};
+void test5()
+{
+    CPU *intercpu = new inter_cpu;
+    Video *intervideo = new inter_vi;
+    memory *intermem = new inter_mem;
+
+    computer *com = new computer(intercpu, intervideo, intermem);
+    com->work();
+    delete com;
+}
+#endif
 int main(int argc, char *argv[])
 {
-#if 0
-    test1();
-#endif
-    //  test2();
-    test3();
+    // test1();
+    // test2();
+    /* 多态案例1*/
+    // test3();
+    /* 多态案例2*/
+    // test4();
+    test5();
     system("pause");
     return 0;
 }
